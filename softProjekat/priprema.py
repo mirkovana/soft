7b8d135a82475093441dfee19f1c93969ee32743
mirkovana <mana81098@gmail.com>
2021-02-04 15:50:51 +0100

    Dobro prepoznaje brojeve na trening slici

--- softProjekat/priprema.py
+++ softProjekat/priprema.py
@@ -15,7 +15,8 @@ import matplotlib.image as mpimg
 from sklearn import datasets
 from sklearn.cluster import KMeans
 #train_image_paths = '.'+os.path.sep+'dataset'+os.path.sep+'trening'+os.path.sep+'Digital-Numbers.png'
-train_image_paths = r'C:\Users\Ana\Desktop\soft\softProjekat\dataset\trening\Digital-Numbers.png'
+#train_image_paths = r'C:\Users\Ana\Desktop\soft\softProjekat\dataset\trening\Digital-Numbers.png'
+train_image_paths = r'C:\Users\Ana\Desktop\soft\softProjekat\dataset\trening\Digital-Numbers - Copy.png'
 
 SERIALIZATION_FOLDER_PATH = '.'+os.path.sep+'serialized_model'+os.path.sep
 
@@ -128,10 +129,12 @@ def select_roi(image_orig, image_bin):
         # print("NAS W", w)
         # print("NAS H", h)
         area = cv2.contourArea(contour)
-        # print("AREAAAA", area)
-        if area > 100 and h < 100 and h > 11 and w > 11:
+        print("AREAAAA", area)
+        if area > 100 and h < 100 and h > 65 and w > 11:
             # kopirati [y:y+h+1, x:x+w+1] sa binarne slike i smestiti u novu sliku
             # oznaciti region pravougaonikom na originalnoj slici sa rectangle funkcijom
+            if w<30:
+                w=46
             region = image_bin[y:y + h + 1, x:x + w + 1]
             regions_array.append([resize_region(region), (x, y, w, h)])
             cv2.rectangle(image_orig, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -164,14 +167,13 @@ def train_or_load_character_recognition_model(train_image_paths, serialization_f
        """
     # TODO - Istrenirati model ako vec nije istreniran, ili ga samo ucitati iz foldera za serijalizaciju
 
-
     image_color = load_image(train_image_paths)
-   #display_image(image_color)
+    # display_image(image_color)
     img = image_bin1(image_gray(image_color))
     dil1 = dilate(img)
 
     image_erode = erode(dil1)
-    #display_image(image_erode)
+    # display_image(image_erode)
     # plt.imshow(image_erode, 'gray')
     # plt.show()
 
@@ -180,10 +182,12 @@ def train_or_load_character_recognition_model(train_image_paths, serialization_f
     # plt.show()
 
     selected_regions, letters = select_roi(image_color.copy(), image_erode)
-    alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+    alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     print("SR", selected_regions)
     display_image(selected_regions)
     print("leee", letters)
+    #for let in letters:
+     #   display_image(let)
     inputs = prepare_for_ann(letters)
 
     outputs = convert_output(alphabet)
